8ef6f79fb10c4349de8caf95304740c05de20f5a
Ignacio Boyadjian <nacho.boya@gmail.com>
2022-07-31 23:43:49 -0300

    Correciones de Any

--- MVT_BoyadjianIgnacio/familia/views.py
+++ MVT_BoyadjianIgnacio/familia/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+
 from django.http import HttpResponse
+
 from familia.models import Familiares
 
 # Create your views here.
@@ -8,9 +10,4 @@ def lista_familia(request):
 
     familiares = Familiares.objects.all()
 
-    lista_familiares = []
-
-    for familiar in familiares:
-        lista_familiares.append[familiar.nombre]
-
-    return HttpResponse(lista_familiares)
+    return render(request, "index.html", {"familia" : familiares})
